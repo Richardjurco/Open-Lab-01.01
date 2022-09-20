9a916c35b9a75d55d6e2c42a2f39b94c20ded145
Richardjurco <113421214+Richardjurco@users.noreply.github.com>
2022-09-20 09:46:18 +0200

    Update Exercise.cs

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_01._01
 {
@@ -6,7 +6,17 @@ namespace Open_Lab_01._01
     {
         public bool IsLessThan10(string input)
         {
-            throw new NotImplementedException();
+            int num_input1 = Int32.Parse(input);
+
+            if (num_input1 < 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+                throw new NotImplementedException();
+            }
         }
     }
 }
